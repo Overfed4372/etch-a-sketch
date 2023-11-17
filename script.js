95267f4e2a415b2d619dc22068c868a6ad4df7f2
Ahmad <ahmad@test.example>
2023-11-17 11:27:58 -0330

    Add 16*16 squares
    16*16 squares are added to the DOM, where each row and column has 30 of them.
    In total, we have 900 squares.

---
+++ script.js
@@ -0,0 +1,14 @@
+function createOneToOneBox () {
+    const box = document.createElement('div');
+    box.setAttribute('style' , `height: 16px; width: 16px; flex: 0 0 auto; box-sizing: border-box ;border: 0px solid #3882f6`);
+    box.setAttribute('class', 'one-to-one-box');
+    return box;
+}
+
+const mainBoxFrame = document.querySelector('.sixteen-to-16-box');
+for (let column = 1 ; column <= 30 ; column++) {
+    for (let row = 1 ; row <= 30 ; row ++) {
+        let littleBox = createOneToOneBox() ; 
+        mainBoxFrame.appendChild(littleBox);
+    }
+} 
