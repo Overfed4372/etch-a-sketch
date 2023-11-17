020afd8c2c5994f9ab51a4ee1b1c27a81877c0fb
Ahmad <ahmad@test.example>
2023-11-17 12:08:30 -0330

    Add color ability to squares
    Squares will have black color after hovering mouse over them using addEventListener().

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function createOneToOneBox () {
     const box = document.createElement('div');
-    box.setAttribute('style' , `height: 16px; width: 16px; flex: 0 0 auto; box-sizing: border-box ;border: 0px solid #3882f6`);
+    box.setAttribute('style' , `height: 16px; width: 16px; flex: 0 0 auto; box-sizing: border-box ;border: 1px solid #e0e0e0`);
     box.setAttribute('class', 'one-to-one-box');
     return box;
 }
 
+const allBoxes = [];
 const mainBoxFrame = document.querySelector('.sixteen-to-16-box');
 for (let column = 1 ; column <= 30 ; column++) {
     for (let row = 1 ; row <= 30 ; row ++) {
-        let littleBox = createOneToOneBox() ; 
+        const littleBox = createOneToOneBox();
+        allBoxes.push(littleBox);
         mainBoxFrame.appendChild(littleBox);
     }
 } 
+allBoxes.forEach ( (box) => {
+    box.addEventListener('mouseover' , (event) => {
+        event.target.style.backgroundColor = 'black';
+    });
+}  );
