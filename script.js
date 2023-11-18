49d98ae2a092136a9cdf2e9ab86e3511c0c77505
Ahmad <ahmad@test.example>
2023-11-18 14:10:51 -0330

    Add grid changing option
    The size of the main sketch is 480*480 px.
    The user has the ability to change the number of pixels in rows and columns.

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-function createOneToOneBox () {
+function createOneToOneBox (scale) {
     const box = document.createElement('div');
-    box.setAttribute('style' , `height: 16px; width: 16px; flex: 0 0 auto; box-sizing: border-box ;border: 1px solid #e0e0e0`);
+    box.setAttribute('style' , `height: ${scale}px; width: ${scale}px; flex: 0 0 auto; box-sizing: border-box ;border: 0px solid #e0e0e0`);
     box.setAttribute('class', 'one-to-one-box');
     return box;
 }
 
-const allBoxes = [];
-const mainBoxFrame = document.querySelector('.sixteen-to-16-box');
-for (let column = 1 ; column <= 30 ; column++) {
-    for (let row = 1 ; row <= 30 ; row ++) {
-        const littleBox = createOneToOneBox();
-        allBoxes.push(littleBox);
-        mainBoxFrame.appendChild(littleBox);
+function createSketch (rowAndColumnNumber = 30) {
+    //The sketch size is 480*480px
+    //rowAndColumnNumber is the number of squares in the row and column
+    //Scale is the size of each square
+    let scale = Math.floor(480 / rowAndColumnNumber);
+    const allBoxes = [];
+    const mainBoxFrame = document.querySelector('.sketch');
+    for (let row = 1 ; row <= rowAndColumnNumber ; row++) {
+        for (let column = 1 ; column <= rowAndColumnNumber ; column ++) {
+            const littleBox = createOneToOneBox(scale);
+            allBoxes.push(littleBox);
+            mainBoxFrame.appendChild(littleBox);
+        }
     }
-} 
-allBoxes.forEach ( (box) => {
-    box.addEventListener('mouseover' , (event) => {
-        event.target.style.backgroundColor = 'black';
-    });
-}  );
+    let mouseDown = false;
+
+    allBoxes.forEach ( (box) => {
+        box.addEventListener('mouseover' , (event) => {
+            // event.target.onmousedown = () => {mouseDown = true};
+            // event.target.onmouseup = () => {mouseDown = false};
+            // if (!mouseDown) return
+            event.target.classList.add('filled-button');
+        });
+
+    }  ); 
+}
+
+function changeTheGrid (rowAndColumnNumber) {
+    const sketch = document.querySelector(".sketch");
+    while (sketch.firstChild) {
+        sketch.removeChild(sketch.firstChild);
+    }
+    createSketch (rowAndColumnNumber);
+}
+
+createSketch();
+const changeTheGridButton = document.querySelector(".change-grid");
+changeTheGridButton.addEventListener("click" , (event) => {
+    let gridNumberInput = parseInt(prompt("Choose the grid number:" , "30"));
+    //Chekc if gridNumberInput is a number
+    ( !isNaN(gridNumberInput) ) && ( changeTheGrid(gridNumberInput) );
+});
